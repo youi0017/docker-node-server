eba0b38e790435b1f6ffd05e06993534547be0ea
root <root@deb11.local.com>
2022-05-11 14:13:42 +0800

    净化页面

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# /usr/bin/php /www/http.php
-
 if [ "${1}" = "node" ]; then
-  # biner="node";
   sh -c  "${*}"
 elif [ "${1}" = "npm" ]; then
-  # biner="npm"
   sh -c  "${*}"
 else
   echo "错误：不提供 非node 或 非npm 的命令：${1}";
